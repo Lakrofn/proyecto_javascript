4478c15bd5adce2198486240de0f5dbe66dee41f
makumbilla <javiermadrigalhidalgoo@gmail.com>
2026-04-20 19:24:39 +0200

    problema arreglado

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.addEventListener('keyup', e => {
+    if(e.target.matches('#myInput')){
+        document.querySelectorAll('.card-dinamica .delantera').forEach(card => {
+            card.textContent.toUpperCase().includes(e.target.value.toUpperCase())
+            ? card.parentElement.style.display = 'block'
+            : card.parentElement.style.display = 'none';   
+        });
+    }
+});
 function mostrarSeccion(id) {
     document.querySelectorAll('.seccion').forEach(sec => {
         sec.classList.remove('activa');
